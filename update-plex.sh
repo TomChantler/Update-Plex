cdbf9ffcd1928536fedd5ac2b2c7f08ebe282d9f
Tom Chantler <tomchantler@users.noreply.github.com>
2022-04-14 11:02:28 +0100

    Working version

---
+++ update-plex.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# tomssl.com/update-plex-server-on-ubuntu-automatically
+dry_run=false
+force_installation=false
+overwrite_file=false
+display_help=false
+
+while getopts 'hfno-:' opt; do
+    case "$opt" in
+        h) display_help=true ;;
+        f) force_installation=true ;;
+        n) dry_run=true ;;
+        o) overwrite_file=true ;;
+        -) case "${OPTARG}" in
+             dry-run) dry_run=true ;;
+             force) force_installation=true ;;
+             help) display_help=true ;;
+             overwrite) overwrite_file=true ;;
+             *) if [ "$OPTERR" = 1 ] && [ "${opt:0:1}" != ":" ]; then
+                        echo "Unknown option --${OPTARG}" >&2
+                        exit 1
+                fi ;;
+           esac ;;
+        *) echo "Error: the only valid options are --dry-run, -n, --force, -f, --help, -h, --overwrite, -o" >&2
+           exit 1
+    esac
+done
+
+if [[ $display_help == true ]]; then
+  echo "usage: $0 -h, --help, -f, --force, -n, --dry-run, -o, --overwrite"
+  exit 2
+fi
+
+echo "Dry Run = $dry_run"
+echo "Force Download = $force_installation"
+echo "Overwrite File = $overwrite_file"
+
+if [[ $EUID -ne 0 ]]; then
+    echo "$0 is not running as root. Try using sudo."
+    exit 2
+fi
+
+rawjson=`curl -s https://plex.tv/api/downloads/5.json?channel=plexpass`
+url=`echo $rawjson | jq -r -c '.computer.Linux.releases | .[] | select(.url | contains("amd64")) | .url'`
+latestversion=`echo $rawjson | jq -r -c '.computer.Linux.version'`
+installedversion=`dpkg -s plexmediaserver | grep -i '^Version' | cut -d' ' -f2`
+echo "Latest version:    $latestversion"
+echo "Installed version: $installedversion"
+echo "------------------------------------------------------------"
+if [ "$installedversion" == "$latestversion" ]; then
+  echo "Already on latest version."
+else
+  echo "Need to upgrade..."
+  echo "Found latest version at $url"
+  filename=${url##*/}
+  if [ -f "$filename" ] && [ "$overwrite_file" == false ]; then
+    echo "File already exists, not going to download it again"
+  else
+    echo "Downloading $filename..."
+    curl -sL -o $filename $url
+  fi
+  if [ "$dry_run" == false ]; then
+    echo "Installing it now..."
+    dpkg -i $filename
+    echo "$filename installed"
+  fi
+fi
+echo "Done"
+exit 0
