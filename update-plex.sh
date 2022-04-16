b5ea79416e7bff46e5b80e040211cc52965033fa
Tom Chantler <tomchantler@users.noreply.github.com>
2022-04-16 02:16:08 +0100

    Fix Beta channel downloads (requires token)

--- update-plex.sh
+++ update-plex.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 # tomssl.com/update-plex-server-on-ubuntu-automatically
+plex_token=PUTYOURSHERE
+beta_channel=false
 dry_run=false
 force_installation=false
 overwrite_file=false
 display_help=false
 
-while getopts 'hfno-:' opt; do
+while getopts 'bhfno-:' opt; do
     case "$opt" in
+        b) beta_channel=true ;;
         h) display_help=true ;;
         f) force_installation=true ;;
         n) dry_run=true ;;
         o) overwrite_file=true ;;
         -) case "${OPTARG}" in
+             beta) beta_channel=true ;;
              dry-run) dry_run=true ;;
              force) force_installation=true ;;
              help) display_help=true ;;
@@ -21,16 +25,17 @@ while getopts 'hfno-:' opt; do
                         exit 1
                 fi ;;
            esac ;;
-        *) echo "Error: the only valid options are --dry-run, -n, --force, -f, --help, -h, --overwrite, -o" >&2
+        *) echo "Error: the only valid options are --beta, -b, --dry-run, -n, --force, -f, --help, -h, --overwrite, -o" >&2
            exit 1
     esac
 done
 
 if [[ $display_help == true ]]; then
-  echo "usage: $0 -h, --help, -f, --force, -n, --dry-run, -o, --overwrite"
+  echo "usage: $0 -h, --help, -b, --beta, -f, --force, -n, --dry-run, -o, --overwrite"
   exit 2
 fi
 
+echo "Beta Channel = $beta_channel"
 echo "Dry Run = $dry_run"
 echo "Force Download = $force_installation"
 echo "Overwrite File = $overwrite_file"
@@ -40,7 +45,12 @@ if [[ $EUID -ne 0 ]]; then
     exit 2
 fi
 
-rawjson=`curl -s https://plex.tv/api/downloads/5.json?channel=plexpass`
+if [ "$beta_channel" == true ]; then
+    rawjson=`curl -s "https://plex.tv/api/downloads/5.json?channel=plexpass" -H "X-Plex-Token: $plex_token"`
+else
+    rawjson=`curl -s "https://plex.tv/api/downloads/5.json"`
+fi
+
 url=`echo $rawjson | jq -r -c '.computer.Linux.releases | .[] | select(.url | contains("amd64")) | .url'`
 latestversion=`echo $rawjson | jq -r -c '.computer.Linux.version'`
 installedversion=`dpkg -s plexmediaserver | grep -i '^Version' | cut -d' ' -f2`
